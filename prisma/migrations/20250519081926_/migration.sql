ac4b0006927ec35f0c74460d5081d5979d21de0f
wasabi <kazue.kirishiki@unknowntech.jp>
2025-05-19 08:51:45 +0000

    models are created

---
+++ prisma/migrations/20250519081926_/migration.sql
@@ -0,0 +1,121 @@
+-- CreateTable
+CREATE TABLE "Profile" (
+    "id" SERIAL NOT NULL,
+    "sex" TEXT NOT NULL,
+    "age" INTEGER NOT NULL,
+    "profileText" TEXT NOT NULL,
+    "userId" INTEGER NOT NULL,
+
+    CONSTRAINT "Profile_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Human" (
+    "id" SERIAL NOT NULL,
+    "sex" TEXT NOT NULL,
+    "age" INTEGER NOT NULL,
+    "address" TEXT NOT NULL,
+    "profileId" INTEGER NOT NULL,
+
+    CONSTRAINT "Human_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "ProfileTag" (
+    "id" SERIAL NOT NULL,
+    "profileId" INTEGER NOT NULL,
+    "tagId" INTEGER NOT NULL,
+
+    CONSTRAINT "ProfileTag_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Tag" (
+    "id" SERIAL NOT NULL,
+    "tag" TEXT NOT NULL,
+
+    CONSTRAINT "Tag_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Photo" (
+    "id" SERIAL NOT NULL,
+    "photo" TEXT NOT NULL,
+    "profileId" INTEGER NOT NULL,
+
+    CONSTRAINT "Photo_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Chat" (
+    "id" SERIAL NOT NULL,
+    "message" TEXT NOT NULL,
+    "time" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "senderId" INTEGER NOT NULL,
+    "recipientId" INTEGER NOT NULL,
+
+    CONSTRAINT "Chat_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Like" (
+    "id" SERIAL NOT NULL,
+    "senderId" INTEGER NOT NULL,
+    "recipientId" INTEGER NOT NULL,
+
+    CONSTRAINT "Like_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Profile_userId_key" ON "Profile"("userId");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Human_profileId_key" ON "Human"("profileId");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "ProfileTag_profileId_key" ON "ProfileTag"("profileId");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "ProfileTag_tagId_key" ON "ProfileTag"("tagId");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Photo_profileId_key" ON "Photo"("profileId");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Chat_senderId_key" ON "Chat"("senderId");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Chat_recipientId_key" ON "Chat"("recipientId");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Like_senderId_key" ON "Like"("senderId");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Like_recipientId_key" ON "Like"("recipientId");
+
+-- AddForeignKey
+ALTER TABLE "Profile" ADD CONSTRAINT "Profile_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Human" ADD CONSTRAINT "Human_profileId_fkey" FOREIGN KEY ("profileId") REFERENCES "Profile"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "ProfileTag" ADD CONSTRAINT "ProfileTag_profileId_fkey" FOREIGN KEY ("profileId") REFERENCES "Profile"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "ProfileTag" ADD CONSTRAINT "ProfileTag_tagId_fkey" FOREIGN KEY ("tagId") REFERENCES "Tag"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Photo" ADD CONSTRAINT "Photo_profileId_fkey" FOREIGN KEY ("profileId") REFERENCES "Profile"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Chat" ADD CONSTRAINT "Chat_senderId_fkey" FOREIGN KEY ("senderId") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Chat" ADD CONSTRAINT "Chat_recipientId_fkey" FOREIGN KEY ("recipientId") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Like" ADD CONSTRAINT "Like_senderId_fkey" FOREIGN KEY ("senderId") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Like" ADD CONSTRAINT "Like_recipientId_fkey" FOREIGN KEY ("recipientId") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
